a96baf27295b4fbfd9f0744a775f3714e3fbdb7c
Nerwin Alamas <nerwinalamas@gmail.com>
2026-03-29 08:37:54 +0800

    feat: orders table

---
+++ supabase/migrations/20260329003004_create_orders_table.sql
@@ -0,0 +1,11 @@
+create table orders (
+  id uuid primary key default gen_random_uuid(),
+  total_price numeric not null,
+  status text default 'pending',
+  created_at timestamptz default now()
+);
+
+alter table orders enable row level security;
+
+create policy "Allow insert orders"
+  on orders for insert with check (true);
